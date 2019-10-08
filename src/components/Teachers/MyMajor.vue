989e4ad1280376119efbd53abcf299ebeae723e0
sn <910205841@qq.com>
2019-10-08 19:29:03 +0800

    0002222000111

--- src/components/Teachers/MyMajor.vue
+++ src/components/Teachers/MyMajor.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div id="main">
-=======
   <div id="">
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
     <div id="navbg">
       <div id="navbg-img">
         <div id="h-name">大前端001</div>
@@ -14,23 +10,6 @@
         </ul>
       </div>
     </div>
-<<<<<<< HEAD
-    <div class="wenben">
-      <p>专业介绍</p>
-      <p>专业章节</p>
-    </div>
-    <div class="class">
-      <el-collapse v-model="activeNames" @change="handleChange">
-        <el-collapse-item title="第一学期" name="1">
-          <div>前端001</div>
-          <div>前端导论</div>
-        </el-collapse-item>
-        <el-collapse-item title="第二学期" name="2">
-          <div>前端002</div>
-        </el-collapse-item>
-        <el-collapse-item title="第三学期" name="3">
-          <div>php基础</div>
-=======
     <div id="c-title">
       <p>专业介绍</p>
       <p>专业章节</p>
@@ -43,7 +22,6 @@
           <div v-for="(items,index) in item.childList" :key="index">
             {{items.name}}
           </div>
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
         </el-collapse-item>
       </el-collapse>
     </div>
@@ -55,63 +33,33 @@ export default {
   data() {
     return {
       //当前组件用到的数据
-<<<<<<< HEAD
-      activeNames: ["1"]
-=======
       activeNames: ['1'],
       arr:[],
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
     };
   },
   methods: {
     //当前组件用到的函数
     handleChange(val) {
-<<<<<<< HEAD
-      console.log(val);
+      console.log('2222')
     }
-=======
-
-      }
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
   },
   created() {
     //组件加载完之后的生命周期函数，如果页面一加载就需要展示数据，那么数据在这获取
     var app = this;
-<<<<<<< HEAD
-    this.$http
-      .get("/product/majorCustom/getMajorCustomForCurrentUser")
-      .then(function(res) {
-        console.log(res);
-      });
-=======
     this.$http.get('/product/userMajorCustom/getTeacherMajorCustomAdapterList').then(function(res){
         app.arr = res.data[0].majorCustomItemTreeAdapterList;
     })
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
   }
 };
 </script>
 <style lang="">
-<<<<<<< HEAD
-.wenben p {
-  border-left: 4px solid #4ac0e0;
-  font-family: 微软雅黑;
-  margin-left: 120px;
-  font-size: 16px;
-}
-=======
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
 #navbg {
   height: 144px;
   background: #89db96;
 }
 #navbg-img {
   height: 144px;
-<<<<<<< HEAD
-  background: url(../../images/download.png) no-repeat 940px -20px;
-=======
   background: url(../../images/download.png) no-repeat 720px -20px;
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
   position: relative;
 }
 #h-name {
@@ -138,10 +86,6 @@ export default {
   margin: 15px 0 12px 125px;
   position: relative;
   border-left: 4px solid#49c0e0;
-<<<<<<< HEAD
-}
-</style>
-=======
 }
 #el-collapse-head-4090 {
   background: #49c0e0;
@@ -149,4 +93,3 @@ export default {
   font-size: 18px;
 }
 </style>
->>>>>>> 9e32dacfafdf192054032a10f86315b2d69adbee
